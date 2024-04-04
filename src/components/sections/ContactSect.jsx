1c153f708b07dfeac009e7ae6266d9418456b075
Akash Sharma <aks.85.sky@gmail.com>
2024-04-04 11:19:57 +0530

    Contact From functionality

--- src/components/sections/ContactSect.jsx
+++ src/components/sections/ContactSect.jsx
@@ -14,12 +14,20 @@ import PhoneInTalkOutlinedIcon from '@mui/icons-material/PhoneInTalkOutlined';
 import MarkEmailUnreadOutlinedIcon from '@mui/icons-material/MarkEmailUnreadOutlined';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import ReCAPTCHA from "react-google-recaptcha";
+import toast, { Toaster } from 'react-hot-toast';
+
 const recaptchaRef = React.createRef();
 
 const ContactSect = () => {
 
     const [siteInfo, setSiteInfo] = useState([]);
     const [products, setProduct] = useState([]);
+    const [error, setError] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [product, setEnquiryProduct] = useState('');
+    const [companyName, setCompanyName] = useState('');
 
     const boxVariant = {
         visible: { opacity: 1, scale: 1, transition: { duration: 0.325 } },
@@ -58,21 +66,64 @@ const ContactSect = () => {
 
     const handleEnquiryForm = async (event) => {
         event.preventDefault();
+
         const formData = event.target;
         const form = new FormData(formData);
         const data = Object.fromEntries(form.entries());
         Object.keys(data).forEach(k => data[k] = data[k].trim());
+        if (
+            data.name === '' ||
+            data.phone === '' ||
+            data.email === '' ||
+            data.companyName === '' ||
+            data.product === ''
+        ) {
+            setError(true);
+            return false;
+        }
+        if (data['g-recaptcha-response'] === '') {
+            toast.error('Click on I\'m not a robot box to verify yourself.', {
+                style: {
+                    borderRadius: '10px',
+                    background: '#2c2b7b',
+                    color: '#fff',
+                }
+            });
+        }
 
         let result = await fetch(`${process.env.REACT_APP_BASE_URL}enquiry`,
             {
                 method: 'POST',
-                data: data,
+                body: JSON.stringify(data),
+                headers: { 'Content-Type': 'application/json' },
             }
         )
+
+        result = await result.json();
+
+        if (result.status) {
+            event.target.reset();
+            toast.success('Thank you for Contacting.', {
+                style: {
+                    borderRadius: '10px',
+                    background: '#2c2b7b',
+                    color: '#fff',
+                }
+            });
+        } else {
+            toast.error(result.message, {
+                style: {
+                    borderRadius: '10px',
+                    background: '#2c2b7b',
+                    color: '#fff',
+                }
+            });
+        }
     }
 
     return (
         <>
+            <Toaster position="bottom-right" />
             <section className='sect-space bg-gry contact-container'>
                 <div className='container'>
 
@@ -96,18 +147,59 @@ const ContactSect = () => {
                                 <form onSubmit={handleEnquiryForm}>
                                     <div className='row'>
                                         <div className='col-lg-6'>
-                                            <TextField label="Your Name" name='name' type='text' />
+                                            <TextField
+                                                error={
+                                                    (error && name == '')
+                                                        ?
+                                                        true
+                                                        :
+                                                        false
+                                                }
+                                                onChange={e => setName(e.target.value)}
+                                                label="Your Name"
+                                                name='name'
+                                                type='text' />
                                         </div>
                                         <div className='col-lg-6'>
-                                            <TextField label="Your Phone" name='phone' type='number' />
+                                            <TextField
+                                                error={
+                                                    (error && phone == '')
+                                                        ?
+                                                        true
+                                                        :
+                                                        false
+                                                }
+                                                onChange={e => setPhone(e.target.value)}
+                                                label="Your Phone"
+                                                name='phone'
+                                                type='number' />
                                         </div>
                                         <div className='col-lg-6'>
-                                            <TextField label="Your Email" name='email' type='email' />
+                                            <TextField
+                                                error={
+                                                    (error && email == '')
+                                                        ?
+                                                        true
+                                                        :
+                                                        false
+                                                }
+                                                onChange={e => setEmail(e.target.value)}
+                                                label="Your Email"
+                                                name='email'
+                                                type='email' />
                                         </div>
                                         <div className='col-lg-6'>
                                             <FormControl fullWidth>
                                                 <InputLabel id="demo-simple-select-label">Select service</InputLabel>
                                                 <Select
+                                                    error={
+                                                        (error && product == '')
+                                                            ?
+                                                            true
+                                                            :
+                                                            false
+                                                    }
+                                                    onChange={e => setEnquiryProduct(e.target.value)}
                                                     label="Select product"
                                                     name='product'
                                                 // onChange={handleChange}
@@ -126,10 +218,24 @@ const ContactSect = () => {
 
                                         </div>
                                         <div className='col-12'>
-                                            <TextField label="Company Name" name='companyName' type='text' />
+                                            <TextField
+                                                error={
+                                                    (error && companyName == '')
+                                                        ?
+                                                        true
+                                                        :
+                                                        false
+                                                }
+                                                onChange={e => setCompanyName(e.target.value)}
+                                                label="Company Name"
+                                                name='companyName'
+                                                type='text' />
                                         </div>
                                         <div className='col-12'>
-                                            <TextField multiline rows={3} labe l="Write any message..." name='message' type='text' />
+                                            <TextField label="Address" name='address' type='text' />
+                                        </div>
+                                        <div className='col-12'>
+                                            <TextField multiline rows={3} label="Write any message..." name='message' type='text' />
                                         </div>
                                         <div className='col-12 mb-2'>
                                             <ReCAPTCHA
